75e36bc916643ba248a60afc447b7a4f1a5f2c42
Sylvain Laconi <sylvain.laconi@gmail.com>
2021-07-04 11:51:02 +0200

    quest-1

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,3 +1,5 @@
+const assert = require("assert");
+
 const capitalizeFirstLetters = (str) => {
   return str.length > 0
     ? str
@@ -7,4 +9,16 @@ const capitalizeFirstLetters = (str) => {
     : "";
 };
 
-module.exports = capitalizeFirstLetters;
+//Check that capitalizeFirstLetters is a function
+assert.strictEqual(typeof capitalizeFirstLetters, "function");
+//Check for empty string
+assert.strictEqual(capitalizeFirstLetters(""), "");
+//Check for one letter
+assert.strictEqual(capitalizeFirstLetters("a"), "A");
+//Check for one word
+assert.strictEqual(capitalizeFirstLetters("coucou"), "Coucou");
+//Check for more than on word
+assert.strictEqual(
+  capitalizeFirstLetters("my name is sylvain"),
+  "My Name Is Sylvain"
+);
